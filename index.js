d7aa4cfd1b4d594a088c146e61cd9ea91eead0aa
Chí Bảo <chibao284@gmail.com>
2021-12-09 10:00:41 +0700

    Apply code into IOS

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
+import codePush from 'react-native-code-push';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () =>
+  codePush({
+    updateDialog: true,
+    installMode: codePush.InstallMode.IMMEDIATE,
+    checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  })(App),
+);
