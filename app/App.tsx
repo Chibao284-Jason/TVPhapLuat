790b8f1963926487e09332d294a105423b58d284
Chí Bảo <chibao284@gmail.com>
2021-12-09 11:34:07 +0700

    StatusBar darkmode

--- app/App.tsx
+++ app/App.tsx
@@ -13,7 +13,7 @@ const {persistor, store} = configureStore();
 const App = (props: AppProps) => {
   return (
     <Provider store={store}>
-      <StatusBar hidden={false} />
+      <StatusBar barStyle={'dark-content'} />
       <PersistGate loading={null} persistor={persistor}>
         <Navigator />
       </PersistGate>
